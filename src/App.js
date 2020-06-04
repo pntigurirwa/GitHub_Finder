90de3019104f435965681dd5ef31ad4d2eeb5c5e
pntigurirwa <pntigurirwa@gmail.com>
2020-06-04 11:05:42 -0500

    List and PAssing State with props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
+
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +9,10 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
