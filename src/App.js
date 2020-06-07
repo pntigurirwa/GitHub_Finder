2cf5d406169dbd008329ffd348131642e1faa6b1
pntigurirwa <pntigurirwa@gmail.com>
2020-06-06 23:40:15 -0500

    React GitHub Project one done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import axios from 'axios';
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-class App extends Component {
-  state = {
-    user: [],
-    loading: false
-  }
-  async componentDidMount() {
-   
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    
-    this.setState({ users: res.data, loading: false });
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
-        </div>
-      </div>
-    );
-  }
-}
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
